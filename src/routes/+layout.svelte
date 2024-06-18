293a83ba161e91664528c385815901e5d71be894
Joseph Call <jbcallv@gmail.com>
2024-06-17 20:42:31 -0400

    made some progress

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,22 @@
 <script>
 	import '../app.css';
+
+	let active = 'home';
 </script>
 
-<slot />
+<div class="flex flex-col h-dvh">
+	<div class="flex">
+		<button class="px-10 py-6" on:click|preventDefault={() => (active = 'home')}>
+			<p class={active === 'home' ? 'text-[#FFFAFA]' : 'text-[#BBBABF]'}>for two.</p>
+		</button>
+
+		<button class="text-[#BBBABF] px-10 py-6" on:click|preventDefault={() => (active = 'features')}>
+			<p class={active === 'features' ? 'text-[#FFFAFA]' : 'text-[#BBBABF]'}>features</p>
+		</button>
+
+		<button class="text-[#BBBABF] px-10 py-6" on:click|preventDefault={() => (active = 'about')}>
+			<p class={active === 'about' ? 'text-[#FFFAFA]' : 'text-[#BBBABF]'}>about</p>
+		</button>
+	</div>
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,31 @@
-<div class="bg-slate-300">Welcome to SvelteKit</div>
+<script>
+	let email = '';
+</script>
+
+<div class="flex h-full justify-center items-center space-x-60">
+	<div class="flex justify-center items-center h-[450px] w-[450px] rounded-[20px] bg-[#211F29]">
+		<div class="flex -space-x-20 space-y-16">
+			<div class="h-[150px] w-[150px] rounded-full bg-[#E26D5C] z-0"></div>
+			<div class="h-[150px] w-[150px] rounded-full bg-[#768844] z-10"></div>
+		</div>
+	</div>
+
+	<div class="max-w-[350px] space-y-12">
+		<div class="leading-[80px]">
+			<p class="text-[80px] text-[#FFFAFA] text-center">for two.</p>
+			<p class="text-[80px] text-[#bbbabfc5] text-center">coming soon.</p>
+		</div>
+
+		<div class="space-y-4">
+			<input
+				class="bg-[#211F29] rounded-[20px] h-[45px] pl-6 w-full outline-none text-[#FFFAFA]"
+				placeholder="email"
+				bind:value={email}
+			/>
+			<button
+				class="bg-[#FABC2A] rounded-[20px] h-[45px] w-full"
+				on:click|preventDefault={() => console.log(email)}>receive news</button
+			>
+		</div>
+	</div>
+</div>
