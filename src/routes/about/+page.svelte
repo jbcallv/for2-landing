6324165f5385b3a51c3f14b2e498c6aa1ce0301b
Joseph Call <jbcallv@gmail.com>
2024-06-20 21:04:05 -0400

    about us page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,6 +6,57 @@
 	/>
 </svelte:head>
 
-<div class="flex h-full justify-center items-center space-x-60">
-	<p class="text-[80px] text-[#FFFAFA] text-center">about: coming soon</p>
+<div class="space-y-72 pt-32 pb-48">
+	<div class="flex justify-center items-center space-x-60 p-80">
+		<div class="h-[150px] w-[150px] rounded-full bg-[#E26D5C] z-0"></div>
+		<p class="text-[80px] text-[#FFFAFA] text-center">rose</p>
+	</div>
+
+	<div class="flex justify-center items-center space-x-60 p-80">
+		<p class="text-[80px] text-[#FFFAFA] text-center">stem</p>
+		<div class="h-[150px] w-[150px] rounded-full bg-[#768844] z-10"></div>
+	</div>
+
+	<div class="flex flex-col justify-center items-center p-96">
+		<p class="text-[80px] text-[#FFFAFA] text-center">for two:</p>
+		<p class="text-[80px] text-[#FFFAFA] text-center">nurture your love story.</p>
+	</div>
+
+	<div class="flex justify-center items-center space-x-60">
+		<div class="flex justify-center items-center p-10 rounded-[20px] bg-[#211F29]">
+			<img src="/demo/match.jpg" alt="home page demo" />
+		</div>
+
+		<div class="max-w-[350px] space-y-12">
+			<p class="text-[80px] text-[#FFFAFA] text-center">match with your so.</p>
+		</div>
+	</div>
+
+	<div class="flex justify-center items-center space-x-60">
+		<div class="max-w-[350px] space-y-12">
+			<p class="text-[80px] text-[#FFFAFA] text-center">share thoughtful memories.</p>
+		</div>
+		<div class="flex justify-center items-center p-10 rounded-[20px] bg-[#211F29]">
+			<img src="/demo/feed.jpg" alt="home page demo" />
+		</div>
+	</div>
+
+	<div class="flex justify-center items-center space-x-60">
+		<div class="flex justify-center items-center p-10 rounded-[20px] bg-[#211F29]">
+			<img src="/demo/profile.jpg" alt="home page demo" />
+		</div>
+
+		<div class="max-w-[350px] space-y-12">
+			<p class="text-[80px] text-[#FFFAFA] text-center">customize your profile.</p>
+		</div>
+	</div>
+
+	<div class="flex justify-center items-center space-x-60">
+		<div class="max-w-[350px] space-y-12">
+			<p class="text-[80px] text-[#FFFAFA] text-center">keep your love story private.</p>
+		</div>
+		<div class="flex justify-center items-center p-10 rounded-[20px] bg-[#211F29]">
+			<img src="/demo/login.jpg" alt="home page demo" />
+		</div>
+	</div>
 </div>
